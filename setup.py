761ae451de8af6440baa7d619938488d24c42e23
Svante Kvarnström <sjk@ankeborg.nu>
2015-05-21 03:15:31 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='sjkabc',
+      version='1.0.0a0',
+      description='ABC music notation parser',
+      author='Svante Kvarnström',
+      author_email='sjk@sjk.io',
+      url='https://github.com/sjktje/sjkabc',
+      py_modules=['sjkabc'],
+)
