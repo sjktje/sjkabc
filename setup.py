0069f0e963ddc0f2fbf5843e43bd8d45bfd8724d
Svante Kvarnström <sjk@ankeborg.nu>
2016-03-04 14:39:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@
 from setuptools import setup
 import sjkabc
 
-setup(name='sjkabc',
-      version=sjkabc.__version__,
-      description='ABC music notation parser',
-      author='Svante Kvarnström',
-      author_email='sjk@sjk.io',
-      url='https://github.com/sjktje/sjkabc',
-      py_modules=['sjkabc'],
+setup(
+    name='sjkabc',
+    version=sjkabc.__version__,
+    packages=['sjkabc'],
+    author=u'Svante Kvarnström',
+    author_email='sjk@sjk.io',
+    description='ABC music notation parser',
+    url='https://github.com/sjktje/sjkabc',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'
+    ]
 )
