713b34da55b752e9eed8cefa7e65f15a3dbdf573
Svante Kvarnström <sjk@ankeborg.nu>
2016-03-12 23:08:39 +0100

    Test for key errors

--- tests/test_header_keys.py
+++ tests/test_header_keys.py
@@ -13,6 +13,8 @@
 
 
 import pytest
+from pytest import raises
+
 from sjkabc.sjkabc import get_id_from_field, get_field_from_id
 
 
@@ -20,8 +22,18 @@ def test_get_id_from_field_returns_correct_id():
     assert get_id_from_field('title') == 'T'
 
 
+def test_get_id_from_field_raises_keyerror():
+    with raises(KeyError):
+        get_id_from_field('baba')
+
+
 def test_get_field_from_id_returns_correct_field():
     assert get_field_from_id('T') == 'title'
+
+
+def test_get_field_from_id_raises_keyerror():
+    with raises(KeyError):
+        get_field_from_id('/')
 
 
 if __name__ == "__main__":
