165033a5dbdee8ab93e83becface8dc178c46a74
Svante Kvarnström <sjk@ankeborg.nu>
2016-03-23 00:58:03 +0100

    Make factory attributes post_generation defs

--- tests/factories.py
+++ tests/factories.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import factory
-from factory import Iterator, Sequence, lazy_attribute
+from factory import Iterator, List, post_generation, Sequence
 from sjkabc.sjkabc import Tune
 
 
@@ -9,44 +9,82 @@ class TuneFactory(factory.Factory):
     class Meta:
         model = Tune
 
-    book = ['The Bible']
-    composer = ['Ed Reavy']
-    discography = ['Best hits']
-    group = ['Group info']
-    history = ['Interesting anecdote.']
-    instruction = ['Instruction']
-    key = ['Gm']
-    note_length = ['1/8']
-    notes = ['A note']
-    origin = ['Sweden']
-    title = [
-        Sequence(lambda n: 'Tune {}'.format(n)),
-        'Test title'
-    ]
-    parts = ['AABB']
-    tempo = ['108']
-    rhythm = [Iterator(['reel', 'jig', 'waltz'])]
-    source = ['John Smith']
-    index = [Sequence(lambda n: '{}'.format(n))]
-    transcription = ['Doctor Who']
     abc = ['|:aaa|bbb|ccc:|']
 
-    @lazy_attribute
-    def title(self):
-        return ['Tune {}'.format(self.index), 'Test title']
+    @post_generation
+    def book(obj, create, extracted, **kwargs):
+        obj.book = ['The Bible']
 
-    @lazy_attribute
-    def file(self):
-        return 'http://tunes.sjk.io/tunes/{}/'.format(self.index)
+    @post_generation
+    def composer(obj, create, extracted, **kwargs):
+        obj.composer = ['Ed Reavy']
 
-    @lazy_attribute
-    def metre(self):
-        available_metres = {
-            'reel': '4/4',
-            'jig': '6/8',
-            'waltz': '3/4'
-        }
-        try:
-            return available_metres[self.rhythm[0]]
-        except:
-            return '4/4'
+    @post_generation
+    def discography(obj, create, extracted, **kwargs):
+        obj.discography = ['Best hits']
+
+    @post_generation
+    def group(obj, create, extracted, **kwargs):
+        obj.group = ['Group info']
+
+    @post_generation
+    def history(obj, create, extracted, **kwargs):
+        obj.history = ['Interesting anecdote.']
+
+    @post_generation
+    def instruction(obj, create, extracted, **kwargs):
+        obj.instruction = ['Instruction']
+
+    @post_generation
+    def key(obj, create, extracted, **kwargs):
+        obj.key = ['Gm']
+
+    @post_generation
+    def note_length(obj, create, extracted, **kwargs):
+        obj.note_length = ['1/8']
+
+    @post_generation
+    def notes(obj, create, extracted, **kwargs):
+        obj.notes = ['A note']
+
+    @post_generation
+    def origin(obj, create, extracted, **kwargs):
+        obj.origin = ['Sweden']
+
+    @post_generation
+    def title(obj, create, extracted, **kwargs):
+        obj.title = Sequence(lambda n: ['Test tune {}'.format(n)])
+        # obj.title = ['Test tune']
+
+    @post_generation
+    def parts(obj, create, extracted, **kwargs):
+        obj.parts = ['AABB']
+
+
+    @post_generation
+    def tempo(obj, create, extracted, **kwargs):
+        obj.tempo = ['108']
+
+    @post_generation
+    def rhythm(obj, create, extracted, **kwargs):
+        obj.rhythm = [Iterator(['reel', 'jig', 'waltz'])]
+
+    @post_generation
+    def source(obj, create, extracted, **kwargs):
+        obj.source = ['John Smith']
+
+    @post_generation
+    def index(obj, create, extracted, **kwargs):
+        obj.index = [Sequence(lambda n: '{}'.format(n))]
+
+    @post_generation
+    def transcription(obj, create, extracted, **kwargs):
+        obj.transcription = ['Doctor Who']
+
+    @post_generation
+    def file(obj, create, extracted, **kwargs):
+        obj.file = [Sequence(lambda n: 'http://tunes.sjk.io/tunes/{}/'.format(n))]
+
+    @post_generation
+    def metre(obj, create, extracted, **kwargs):
+        obj.metre = ['4/4']
